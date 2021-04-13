06db019daaf49bcee9e1551f6e703f9972b03ccd
lichin-lin <vic20087cjimlin@gmail.com>
2021-04-13 19:09:30 +0800

    [FEAT] add on/offline notification by package

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,35 +1,20 @@
 import React from "react";
 import ThemeIcon from "./ThemeIcon";
+import useOnlineStatus from "@rehooks/online-status";
 
 function Header({ theme, handleSetTheme }) {
-  const [network, setNetwork] = React.useState("offline");
+  const onlineStatus = useOnlineStatus();
 
-  const mappingStatus = () => {
-    switch (network) {
-      case "online":
-        return "green";
-      case "offline":
-        return "red";
+  const mappingStatus = (onlineStatus) => {
+    switch (onlineStatus) {
+      case true:
+        return "bg-green-500";
+      case false:
+        return "bg-red-500";
       default:
-        return "yellow";
+        return "bg-yellow-500";
     }
   };
-  React.useEffect(() => {
-    window.addEventListener("load", () => {
-      function handleNetworkChange(event) {
-        if (navigator.onLine) {
-          document.body.classList.remove("offline");
-          setNetwork("online");
-        } else {
-          document.body.classList.add("offline");
-          setNetwork("offline");
-        }
-      }
-      window.addEventListener("online", handleNetworkChange);
-      window.addEventListener("offline", handleNetworkChange);
-      handleNetworkChange()
-    });
-  }, []);
 
   return (
     <div className="h-12 px-4 bg-white dark:bg-gray-900 shadow-md flex justify-between items-center">
@@ -38,8 +23,10 @@ function Header({ theme, handleSetTheme }) {
       </div>
       <ThemeIcon theme={theme} handleSetTheme={handleSetTheme} />
       <div className="status flex font-normal dark:text-white">
-        <p>{network}</p>
-        <div className={`dot rounded w-2 h-2 bg-${mappingStatus()}-500`}></div>
+        <p>{onlineStatus ? "online" : "offline"}</p>
+        <div
+          className={`dot rounded w-2 h-2 ${mappingStatus(onlineStatus)}`}
+        ></div>
       </div>
     </div>
   );
